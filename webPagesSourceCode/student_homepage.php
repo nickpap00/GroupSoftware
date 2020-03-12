146a1083f74eaeb0596dc901ed6532921e16288a
jameskitt234 <57361252+jameskitt234@users.noreply.github.com>
2020-03-12 04:23:10 +0000

    Update student_homepage.php
    Add welcome message to student home page

--- webPagesSourceCode/student_homepage.php
+++ webPagesSourceCode/student_homepage.php
@@ -2,26 +2,27 @@
 <link rel="stylesheet" type="text/css" href="styles_coursework.css"></link>
 	<head>
 		<script src="jquery-3.2.1.min.js"></script>
-		
-		<?php 
+
+		<?php
 			include_once("scriptandstyles.php");
 			include_once("userClassObject.php");
 			session_start();
 		?>
-			
+
 	</head>
-	
-	
+
+
 	<body>
 		<div>
 			<center><h1>STUDENT HOMEPAGE</h1></center>
 		</div>
-		
+
 		<?php include_once("navbar.php"); ?> <!-- Includes the navigation bar -->
-		
-		
-		<div id="footer">
-			<?php include_once("footer.php"); ?>
-		</div>
+
+		<p>Welcome to Shortcut Exeter!.</p>
+		<p>Shortcut is your best friend when it comes to getting to know the university.</p>
+		<p>By following our tours you will learn much more about the university and
+			its buildings than just walking around will ever teach you</p>
+
 	</body>
 </html>
